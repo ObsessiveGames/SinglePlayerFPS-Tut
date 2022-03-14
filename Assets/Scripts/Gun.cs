88728ea4fc099babe4e42ffa0e0e2e70e4f72fb7
Jannie <novamancer@gmail.com>
2022-03-14 21:28:19 +0200

    How to make a Singleplayer FPS Game in Unity – Part 2

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,16 +7,20 @@ public class Gun : MonoBehaviour
 {
     private float range = 100f;
     public Camera fpsCam;
+    private AudioSource gunSound;
 
     private void Awake()
     {
         InputManager inputManager = new InputManager();
         inputManager.Player.Enable();
         inputManager.Player.Shoot.performed += Shoot;
+        gunSound = GetComponent<AudioSource>();
     }
 
     private void Shoot(InputAction.CallbackContext context)
     {
+        gunSound.Play();
+        CinemachineShake.instance.ShakeCamera(1f, .25f);
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
